55cd897f82c5015f3ae984c918a2eafc7a9644b2
Alex Tristram <allex.tristram@gmail.com>
2021-06-14 16:31:16 +0100

    add landingScreen data with readable time and an else statement for when terminal equals null

--- src/components/Screens/LandingScreen.vue
+++ src/components/Screens/LandingScreen.vue
@@ -1,28 +1,43 @@
 <template>
   <div>
-  <h1>Landing Screen</h1>
-  <li v-for="content in contents" :key="content.id">
-    <div>{{content.flight_date}}</div>
-  </li>
-</div>
+    <h1>Todays top 10 flights</h1>
+
+    <li v-for="content in contents" :key="content.id">
+      <!-- <div>{{ content.aircraft }}</div> -->
+      <div>{{ content.airline.name }}</div>
+      <div>Departed from {{ content.departure.airport }}</div>
+      <div>
+        Arriving at {{ content.arrival.airport }} at
+
+        {{ new Date(content.arrival.estimated).toString().slice(15, 21) }}
+      </div>
+      <div v-if="content.arrival.terminal === null">
+        Terminal is not yet availbale
+      </div>
+      <div v-else>Terminal {{ content.arrival.terminal }}</div>
+    </li>
+  </div>
 </template>
 
-
 <script>
-  import axios from 'axios'
-  export default {
-    name: "LandingScreen",
-    data() {
-      return {
-        contents: null
-      }
-    },
-    mounted() {
-      axios.get("http://api.aviationstack.com/v1/flights?access_key=bd2207c91f3b326f64fdfe54bc2c4af6&limit=10")
-        .then((response) => {this.contents = response.data.data
+import axios from "axios";
+export default {
+  name: "LandingScreen",
+  data() {
+    return {
+      contents: null,
+    };
+  },
+  mounted() {
+    axios
+      .get(
+        "http://api.aviationstack.com/v1/flights?access_key=bd2207c91f3b326f64fdfe54bc2c4af6&limit=10"
+      )
+      .then((response) => {
+        this.contents = response.data.data;
 
-          console.log(response.data.data)
-        })
-    }
-  };
+        console.log(response.data.data);
+      });
+  },
+};
 </script>
