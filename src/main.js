e499b23ab9605a7f4a7bc35dfc4cc45bc50a412e
Nadia <nadia.m.amir@gmail.com>
2021-06-16 13:57:56 +0100

    create router obj for routing

--- src/main.js
+++ src/main.js
@@ -1,20 +1,43 @@
+import Vue from "vue";
+import App from "./App.vue";
+import VueRouter from 'vue-router'
+import LandingScreen from "./components/Screens/LandingScreen.vue"
+import Airlines from "./components/Screens/Airlines.vue"
+import random from "./components/Screens/random.vue"
+
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import "bootstrap/dist/css/bootstrap.css";
-import Vue from "vue";
-import App from "./App.vue";
-import Router from './Router/index';
+
 
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue);
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin);
-Vue.use(Router);
+Vue.use(VueRouter);
 
 Vue.config.productionTip = false;
 
+const router = new VueRouter({
+  mode: 'history',
+  routes:[
+    {
+      path: '/',
+      component: LandingScreen
+    },
+    {
+      path: '/Airlines',
+      component: Airlines
+    },
+    {
+      path: '/random',
+      component: random
+    },
+  ]
+})
+
 new Vue({
   render: (h) => h(App),
-  Router,
+  router,
 }).$mount("#app");
